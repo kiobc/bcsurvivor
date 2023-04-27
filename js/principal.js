c7e75225e3da1939914b5d6811216c19f96675d8
kiobc <brayan.calderon3464@utc.edu.ec>
2023-04-26 22:52:45 -0500

    Agregado objetos al mapa

--- js/principal.js
+++ js/principal.js
@@ -8,14 +8,18 @@ export default class Principal extends Phaser.Scene {
         Jugador.preload(this);
         this.load.image('tiles','./assets/imagenes/RPG Nature Tileset.png');
         this.load.tilemapTiledJSON('map','./assets/imagenes/mapa.json');
+        this.load.atlas('resources', 'assets/imagenes/recursos.png', 'assets/imagenes/recursos_atlas.json');
     }
     create(){
-        const map = this.make.tilemap({key:'map'});
+        const map = this.make.tilemap({key: 'map'});
+        this.map = map;
         const tileset = map.addTilesetImage('RPG Nature Tileset','tiles',32,32,0,0);
         const suelo = map.createLayer('Tile Layer 1', tileset, 0, 0);
         const suelo1 = map.createLayer('Tile Layer 2', tileset, 0, 0);
         suelo.setCollisionByProperty({collides:true});
         this.matter.world.convertTilemapLayer(suelo);
+        
+        this.addRecursos();
         this.player = new Jugador({scene:this,x:100, y:100,texture:'mujer', frame:'townsfolk_f_idle_1'});
         this.player.inputKeys=this.input.keyboard.addKeys({
             up:Phaser.Input.Keyboard.KeyCodes.W,
@@ -24,6 +28,24 @@ export default class Principal extends Phaser.Scene {
             right:Phaser.Input.Keyboard.KeyCodes.D,
         });
     }
+addRecursos(){
+    const recursos=this.map.getObjectLayer('Resources');
+    recursos.objects.forEach(recurso=>{
+        let resItem=new Phaser.Physics.Matter.Sprite(this.matter.world,recurso.x,recurso.x,'resources',recurso.type);
+        let yOrigin= recurso.properties.find(p=>p.name=='yOrigin').value;
+        resItem.x +=resItem.width/2;
+        resItem.y +=resItem.height/2;
+        resItem.y = resItem.y + resItem.height*(yOrigin-0.5);
+        const{Body,Bodies}=Phaser.Physics.Matter.Matter;
+        var circuloCollider= Bodies.circle(resItem.x,resItem.y,12,{isSensor:false,label:'collider'});
+        resItem.setExistingBody(circuloCollider);
+
+        resItem.setStatic(true);
+        resItem.setOrigin(0.5, yOrigin);
+        this.add.existing(resItem);
+    });
+}
+
 update(){
 this.player.update();
 }
