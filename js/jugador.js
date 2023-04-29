059020c008db8a5f0b27cfbefb2d5776a51c424b
kiobc <brayan.calderon3464@utc.edu.ec>
2023-04-28 21:21:40 -0500

    Agregado arma

--- js/jugador.js
+++ js/jugador.js
@@ -3,6 +3,11 @@ export default class Jugador extends Phaser.Physics.Matter.Sprite {
         let {scene,x,y,texture,frame}=data;
         super(scene.matter.world,x,y,texture,frame);
         this.scene.add.existing(this);
+        //arma
+        this.spriteWeapon = new Phaser.GameObjects.Sprite(this.scene, 0, 0, 'items', 162);
+        this.spriteWeapon.setScale(0.8);
+        this.spriteWeapon.setOrigin(0.25, 0.75);
+        this.scene.add.existing(this.spriteWeapon);
         const{Body,Bodies}=Phaser.Physics.Matter.Matter;
         var jugadorCollider= Bodies.circle(this.x,this.y,12,{isSensor:false,label:'jugadorCollider'});
         var jugadorSensor= Bodies.circle(this.x,this.y,24,{isSensor:true,label:'jugadorSensor'});
@@ -12,11 +17,13 @@ export default class Jugador extends Phaser.Physics.Matter.Sprite {
         });
         this.setExistingBody(compoundBody);
         this.setFixedRotation();
+        this.scene.input.on('pointermove',pointer=>this.setFlipX(pointer.worldX < this.x));
     }
 static preload(scene){
     
     scene.load.atlas('mujer', 'assets/imagenes/mujer.png', 'assets/imagenes/mujer_atlas.json');
     scene.load.animation('mujer_anim', 'assets/imagenes/mujer_anim.json');
+    scene.load.spritesheet('items', 'assets/imagenes/items.png', { frameWidth: 32, frameHeight: 32 });
 }
 
 get velocity(){
@@ -45,5 +52,23 @@ get velocity(){
         } else {
             this.anims.play('mujer_idle',true);
         }   
+        this.spriteWeapon.setPosition(this.x, this.y);
+        this.armaRotar();
+    }
+    armaRotar(){
+        let pointer=this.scene.input.activePointer;
+        if(pointer.isDown){
+            this.armaRotacion +=6;
+        }else{
+            this.armaRotacion =0;
+        }
+        if(this.armaRotacion > 100){
+            this.armaRotacion =0;
+        }
+        if(this.flipX){
+            this.spriteWeapon.setAngle(-this.armaRotacion - 90);
+        }else{
+            this.spriteWeapon.setAngle(this.armaRotacion);
+        }
     }
 }
